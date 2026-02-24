160d28c567d008f687040708fe0b36fa73a5b947
NaveenMotika12 <naveenmotika9652@gmail.com>
2026-02-24 12:48:46 +0530

    fix: disable no-namespace lint for Express type augmentation

--- contracts/src/middleware/requestId.ts
+++ contracts/src/middleware/requestId.ts
@@ -2,6 +2,7 @@ import type { Request, Response, NextFunction } from "express";
 import { randomUUID } from "crypto";
 
 declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace Express {
     interface Request {
       requestId?: string;
